00fae6f2ed09e21f81f063738dd3ed5e9ef9e384
SAMJOIN <nikita.korneev.2222@mail.ru>
2023-10-18 02:16:04 +0300

    ReleaseV

--- src/api/weather-api.js
+++ src/api/weather-api.js
@@ -5,7 +5,7 @@ const weatherAPI = {
 
     async getWeather(city) {
         try {
-            const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?q=${city}&days=7&key=ae8b472c16094750b43133918232109`);
+            const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?q=${city}&days=4&key=ae8b472c16094750b43133918232109`);
             return response;
         } catch (error) {
             console.log(error);
